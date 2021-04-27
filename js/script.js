25327f022bf571f7e62a7497071d8069d8ab6279
akostkasmith <aaron@firstpagemarketing.com>
2021-04-27 08:20:17 -0700

    closing of all panels when one is open

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => panel.addEventListener('click', toggleClick));
+panels.forEach(panel => panel.addEventListener('transitionend', startTransition));
 
 function toggleClick(e) {
-    panels.forEach(panel => panel.classList.remove('open'))
-    e.target.classList.add('open');
+    const classArray = Array.from(e.target.classList);
+    if (!classArray.includes('open')) panels.forEach(panel => panel.classList.remove('open'))
+    e.target.classList.toggle('open');
+}
+
+function startTransition(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
 }
